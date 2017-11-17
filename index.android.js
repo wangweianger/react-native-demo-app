7617e2813e3c07a838ae8f66b25773fbea487539
wangweianger <752636052@qq.com>
2017-11-17 19:36:18 +0800

    xz

---
+++ index.android.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import Zane from './src/main'
+
+AppRegistry.registerComponent('Zane', () => Zane);
---
+++ src/main.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Zane extends Component {
+  render() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>
+                hello world! this is my first page.
+            </Text>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
